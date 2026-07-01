b7b3d368bf1ce93d1bbeeba74bf8fc2d274effbe
HUỲNH THANH DANH <huynhtoan166714@gmail.com>
2026-07-01 01:50:34 -0800

    Configure GitHub Pages deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from './contexts/ThemeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ThemeProvider>
         <App />
       </ThemeProvider>
